3f281b3df717a68ebf0ba5360e7475ec38fc7120
fmrico <fmrico@users.noreply.github.com>
2025-07-26 01:38:59 +0000

    deploy: f2387c5c8a522528cf49149a1ec3a5651dfa78e4

---
+++ search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['install_0',['Install',['../index.html#autotoc_md1',1,'']]]
+];
---
+++ search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['easynav_5findoor_5ftestcase_0',['easynav_indoor_testcase',['../index.html',1,'']]]
+];
